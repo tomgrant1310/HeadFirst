26db970ecc0271d3104d344d7187d6e48ea7dbb4
Tom Grant <tomgrant1310@gmail.com>
2015-11-20 12:42:46 +0000

    add files

---
+++ chapter5/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>A loop of your own</title>
+    <link type='text/css' rel='stylesheet' href='style.css'/>
+</head>
+<body>
+<?php
+//Add while loop below
+$loopCount = 0;
+while (loopCount<4){
+echo "<p>Interation number: {$loopCount} </p>";
+$loopCount ++;
+?>
+</body>
+</html>
